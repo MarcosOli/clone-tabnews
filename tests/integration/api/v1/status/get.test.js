69e20646b85ada79797b7c799009a98746aab7ab
MarcosOli <1990mar@gmail.com>
2024-10-07 22:32:12 +0000

    add database.js and .env files

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,6 @@
 test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("https://producttab.com.br/api/v1/status");
+  const response = await fetch(
+    "https://shiny-winner-qvpggq797pj34r47-3000.app.github.dev/api/v1/status",
+  );
   expect(response.status).toBe(200);
 });
